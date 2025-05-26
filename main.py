8b342471dc83504845d698d36642825097df7de1
Sesha503 <78253774+Sesha503@users.noreply.github.com>
2025-05-26 16:29:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import fetch
 import autolive
 import start
-#my api= AIzaSyBOMzh5Uvp2RNhFdn1Z7Br2D2bVc6Nyvrs
+my_api= 'AIzaSyBOMzh5Uvp2RNhFdn1Z7Br2D2bVc6Nyvrs'
 
 fbi="UCvLSQGgj08vC0ws_9f-Eung"
 
@@ -11,7 +11,7 @@ def main():
     vid_id=autolive.check_live_stream(fbi)
     
     if vid_id:
-        strt_time = start.get_start_time_ist(vid_id)
+        strt_time = start.get_start_time_ist(vid_id,my_api)
         print(strt_time)
         fetch.read(vid_id,strt_time)
         
