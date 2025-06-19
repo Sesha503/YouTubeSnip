9d80c16e526359f6000b55560e085d18564acee1
Sesha503 <78253774+Sesha503@users.noreply.github.com>
2025-06-19 16:34:21 +0530

    Update autolive.py

--- autolive.py
+++ autolive.py
@@ -2,7 +2,7 @@ from googleapiclient.discovery import build
 import os
 
 my_api= os.getenv('API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
+youtube = build('youtube', 'v3', developerKey=my_api)
 
 def check_live_stream(chnl_id):
     request = youtube.search().list(
