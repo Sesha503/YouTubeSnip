8174645f59a03c3a845f018bf4d8d6250926f1e1
Sesha503 <78253774+Sesha503@users.noreply.github.com>
2025-09-01 03:24:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ my_api= os.getenv('API_KEY')
 def main():
     print("Hello from stream-snip!")
     
-    vid_id=autolive.check_live_stream(os.getenv('REY_CHANNEL'))
+    vid_id=autolive.check_live_stream(os.getenv('CHANNEL_ID'))
     
     if vid_id:
         strt_time = start.get_start_time_ist_api(vid_id,my_api)
